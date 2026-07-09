8fde66ef1ca40b56bfa94dd81bd467457ce9aab2
rizq yazed <rizqyazed@gmail.com>
2026-07-09 13:26:52 +0100

    added content system and public site updates

--- app/models/content_block.rb
+++ app/models/content_block.rb
@@ -1,3 +1,9 @@
 class ContentBlock < ApplicationRecord
   belongs_to :site
+
+  # The rich text editor for your text blocks
+  has_rich_text :body
+
+  # The file attachment for your images/audio/video
+  has_one_attached :media_file
 end
--- app/models/site.rb
+++ app/models/site.rb
@@ -1,3 +1,7 @@
 class Site < ApplicationRecord
   belongs_to :user
+
+  has_many :content_blocks, dependent: :destroy
+
+  attr_accessor :folder_upload
 end
